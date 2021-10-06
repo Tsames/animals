a3680450109d6b5540c2645ec73d18302648cf98
tom <tsames1@gmail.com>
2021-10-05 17:28:34 -0700

    Added all routes!

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const animalSchema = new Schema({
     lifeExpectancy: Number
 })
 
-//make fruit model
+//make Animal model
 const Animal = model("Animal", animalSchema)
 
 /*********************************
@@ -49,13 +49,76 @@ app.use(morgan("tiny"))
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
+app.set('view engine', 'ejs')
 
 /*********************************
 Routes
 *********************************/
-
+//Server Landing Page
 app.get("/", (req,res) => {
     res.send("your server is running")
+})
+
+//Seed Route
+app.get("/animals/seed", (req,res) => {
+    const noahsArc = [
+        { species: "Asian Elephant", extinct: false, location: "Indian subcontinent and Southeast Asia.", lifeExpectancy: 60 },
+        { species: "Great White Shark", extinct: false, location: "in the deep end of the pool", lifeExpectancy: 70 },
+        { species: "Bald Eagle", extinct: false, location: "North America", lifeExpectancy: 28 }
+    ];
+
+    Animal.remove({}, (err,data) => {
+        Animal.create(noahsArc, (err,data) => {
+            res.json(data);
+        })
+    })
+})
+
+//Index Route
+app.get("/animals", (req,res) => {
+    Animal.find({}, (err, allAnimals) => {
+        res.render("animals/index", { allAnimals })
+    })
+})
+
+//New Route
+app.get("animals/new", (req,res) => {
+    res.render("animals/new");
+})
+
+//Delete Route
+app.delete("animals/:id", (req,res) => {
+    Animal.findByIdAndRemove(req.body.id, (err, animal) => {
+        res.redirect("/animals");
+    })
+})
+
+//Update Route
+app.put("/animals/:id", (req,res) => {
+    Animal.findByIdAndUpdate(req.body.id, { new:true }, (err, animal) => {
+        res.redirect("/animals");
+    })
+})
+
+//Create Route
+app.post("/animals", (req,res) => {
+    Animal.create(req.body, (err,animal) => {
+        res.redirect("/animals");
+    })
+})
+
+//Edit Route
+app.get("/animals/:id/edit", (req,res) => {
+    Animal.findById(req.params.id, (err, animal) => {
+        res.render("animal/edit", { animal })
+    })
+})
+
+//Show Route
+app.get("animals/:id", (req,res) => {
+    Animal.findById(req.param.id, (err,data) => {
+        res.render("animals/show")
+    })
 })
 
 /*********************************
